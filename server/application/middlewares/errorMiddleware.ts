fe331739446b0da27e4557aaa282d8bc276956ec
Mauro Oliveira <mauro.oliveira@tbdc.com.br>
2022-09-06 14:09:12 -0300

    🥅 creating custom errors to be treated in error middleware

--- server/application/middlewares/errorMiddleware.ts
+++ server/application/middlewares/errorMiddleware.ts
@@ -1,7 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 
+import { CustomError } from '@domain/models';
+
 export default function errorMiddleware(error: unknown, _req: Request, res: Response, next: NextFunction) {
   if (!error) next();
+
+  if (error instanceof CustomError) {
+    return res.status(error.httpStatus).send({ message: error.message });
+  }
 
   return res.status(500).send({ message: 'Internal Server Error ' });
 }
